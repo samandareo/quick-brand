9cb69543d16c9d629a0bf00daf41fddebd4cd000
samandareo <smdeveloper04@gmail.com>
2025-09-27 20:49:47 +0500

    fix: update recovery attempts condition to include equal check for limit

--- controllers/recoveryController.js
+++ controllers/recoveryController.js
@@ -19,7 +19,7 @@ exports.recovery = async (req, res) => {
 
         let recoveryRecord = await Recovery.findOne({ phoneNumber });
         if (recoveryRecord) {
-            if (recoveryRecord.attempts > 5) {
+            if (recoveryRecord.attempts >= 5) {
 
                 const authToken = user.generateAuthToken(true);
                 return ApiResponse.success({ token: authToken, recoveryFailed: true }, "Too many recovery attempts. Please try to recover with support.").send(res);
